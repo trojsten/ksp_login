b8d67dd00e7061a90cb781c2a9e8167edf57ee78
Michal Petrucha <michal.petrucha@ksp.sk>
2012-10-12 17:50:30 +0200

    ksp_login: moved the social_auth monleypatch into __init__.py

--- ksp_login/views.py
+++ ksp_login/views.py
@@ -1,7 +1,5 @@
-from collections import OrderedDict
 from django.shortcuts import render
 from django.core.urlresolvers import reverse
-from django.utils.translation import ugettext_lazy as _
 from django.shortcuts import redirect
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth.decorators import login_required
@@ -69,38 +67,3 @@ def register(request):
     return render(request, "ksp_login/registration.html", {
         'form': form,
     })
-
-
-def __activate_social_auth_monkeypatch():
-    from social_auth.backends import (OPENID_ID_FIELD, SocialAuthBackend,
-        OpenIDBackend)
-    from social_auth.backends.contrib.livejournal import (
-        LIVEJOURNAL_USER_FIELD, LiveJournalBackend)
-    from social_auth.backends.yahoo import YahooBackend
-    from social_auth.backends.google import GoogleBackend
-    from social_auth.backends.contrib.yandex import YandexBackend
-    from social_auth import backends
-
-    SocialAuthBackend.REQUIRED_FIELD_NAME = None
-    SocialAuthBackend.REQUIRED_FIELD_VERBOSE_NAME = None
-
-    OpenIDBackend.REQUIRED_FIELD_NAME = OPENID_ID_FIELD
-    OpenIDBackend.REQUIRED_FIELD_VERBOSE_NAME = _('OpenID identity')
-
-    LiveJournalBackend.REQUIRED_FIELD_NAME = LIVEJOURNAL_USER_FIELD
-    LiveJournalBackend.REQUIRED_FIELD_VERBOSE_NAME = _('LiveJournal username')
-
-    # Reset to None in those OpenID backends where nothing is required.
-    GoogleBackend.REQUIRED_FIELD_NAME = None
-    GoogleBackend.REQUIRED_FIELD_VERBOSE_NAME = None
-    YahooBackend.REQUIRED_FIELD_NAME = None
-    YahooBackend.REQUIRED_FIELD_VERBOSE_NAME = None
-    YandexBackend.REQUIRED_FIELD_NAME = None
-    YandexBackend.REQUIRED_FIELD_VERBOSE_NAME = None
-
-    # We replace the regular dict in social_auth.backends.BACKENDSCACHE
-    # with an OrderedDict in order to remember the order in which they
-    # were imported based on settings.AUTHENTICATION_BACKENDS.
-    backends.BACKENDSCACHE = OrderedDict()
-
-__activate_social_auth_monkeypatch()
