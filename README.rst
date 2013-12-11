6ab216fb312aa76e8aa720d6bf56cf4d26bb88cd
Michal Petrucha <michal.petrucha@ksp.sk>
2013-12-11 20:15:15 +0100

    Added a license, README, updated setup.py.

---
+++ README.rst
@@ -0,0 +1,23 @@
+=========
+KSP Login
+=========
+
+KSP Login is a Django_ app intended as an all-in-one solution for
+authentication. It is built mostly on top of the fantastic
+`python-social-auth`_ library which does most of the heavy lifting.
+
+
+Features
+========
+
+* A complete frontend, just include a few templates and you're good to go.
+
+* Simple management of user-related data.
+
+* Allows both social and traditional username/password authentication.
+
+More features are coming soon (such as user merging etc).
+
+.. _Django: https://djangoproject.com/
+
+.. _python-social-auth: https://github.com/omab/python-social-auth
--- setup.py
+++ setup.py
@@ -1,10 +1,45 @@
+from os.path import join, dirname
 from distutils.core import setup
 
+
+LONG_DESCRIPTION = """
+KSP Login is an app for easy authentication management with support for
+social as well as traditional authentication.
+"""
+
+
+def long_description():
+    """Return long description from README.rst if it's present
+    because it doesn't get installed."""
+    try:
+        return open(join(dirname(__file__), 'README.rst')).read()
+    except IOError:
+        return LONG_DESCRIPTION
+
+
 setup(
-    name='ksp_login',
+    name='ksp-login',
     version='0.1',
     author='Michal Petrucha',
     author_email='michal.petrucha@ksp.sk',
-    url='ttps://github.com/koniiiik/ksp_login',
-    packages=['ksp_login'],
+    url='https://github.com/koniiiik/ksp_login',
+    packages=['ksp_login', 'ksp_login.templatetags'],
+    package_data={
+        'ksp_login': [
+            'templates/ksp_login/*.html',
+            'templates/ksp_login/parts/*.html',
+            'static/ksp_login/img/*.svg',
+            'static/ksp_login/img/*.png',
+            'static/ksp_login/img/*.txt',
+            'static/ksp_login/css/*.css',
+            'static/ksp_login/js/*.js',
+        ],
+    },
+    license='LICENSE',
+    description='A Django app for both traditional and social authentication',
+    long_description=long_description(),
+    install_requires=[
+        'Django>=1.4',
+        'python-social-auth',
+    ],
 )
