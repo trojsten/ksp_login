f92287cedaa2647f3e57e9308a5e6c2a65c6232d
Michal Petrucha <michal.petrucha@koniiiik.org>
2017-01-07 19:52:30 +0100

    Added Django 1.10 to the list of classifiers.

--- setup.py
+++ setup.py
@@ -59,6 +59,7 @@ setup(
         'Framework :: Django',
         'Framework :: Django :: 1.8',
         'Framework :: Django :: 1.9',
+        'Framework :: Django :: 1.10',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python :: 2',
