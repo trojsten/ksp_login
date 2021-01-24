d16011cc1c984e25c8c5c902c21cd6b373c25b05
Michal Hozza <mhozza@gmail.com>
2021-01-24 17:39:28 +0000

    Remove deprecated OpenId providers.

--- ksp_login/__init__.py
+++ ksp_login/__init__.py
@@ -8,9 +8,6 @@ def __activate_social_auth_monkeypatch():
     from social_core.backends.base import BaseAuth
     from social_core.backends.open_id import (OPENID_ID_FIELD, OpenIdAuth)
     from social_core.backends.livejournal import LiveJournalOpenId
-    from social_core.backends.yahoo import YahooOpenId
-    from social_core.backends.google import GoogleOpenId
-    from social_core.backends.yandex import YandexOpenId
 
     BaseAuth.REQUIRED_FIELD_NAME = None
     BaseAuth.REQUIRED_FIELD_VERBOSE_NAME = None
@@ -20,14 +17,6 @@ def __activate_social_auth_monkeypatch():
 
     LiveJournalOpenId.REQUIRED_FIELD_NAME = 'openid_lj_user'
     LiveJournalOpenId.REQUIRED_FIELD_VERBOSE_NAME = _('LiveJournal username')
-
-    # Reset to None in those OpenID backends where nothing is required.
-    GoogleOpenId.REQUIRED_FIELD_NAME = None
-    GoogleOpenId.REQUIRED_FIELD_VERBOSE_NAME = None
-    YahooOpenId.REQUIRED_FIELD_NAME = None
-    YahooOpenId.REQUIRED_FIELD_VERBOSE_NAME = None
-    YandexOpenId.REQUIRED_FIELD_NAME = None
-    YandexOpenId.REQUIRED_FIELD_VERBOSE_NAME = None
 
 
 __activate_social_auth_monkeypatch()
